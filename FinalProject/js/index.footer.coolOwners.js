ccf08488b3bd4846c9a0d2a9d5c7b63c6e6d9792
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-05-22 01:26:24 +0300

    Update index.footer.coolOwners.js

--- FinalProject/js/index.footer.coolOwners.js
+++ FinalProject/js/index.footer.coolOwners.js
@@ -2,29 +2,28 @@ let container = document.querySelector(".mainFooterLink");
 
 container.addEventListener("click", () => {
     let mainContainer = document.querySelector(".footer__links");
-    if (!container.classList.contains("clicked")) {
-        let newElement = document.createElement("ul");
-        newElement.className = "footer__links";
-        
-        const nameElement1 = document.createElement("li");
-        nameElement1.className = "footer__link";
-        nameElement1.textContent = "Oleksandr Nykyforchyn";
-        newElement.appendChild(nameElement1);
-        
-        const nameElement2 = document.createElement("li");
-        nameElement2.className = "footer__link";
-        nameElement2.textContent = "Vlad Karanevych";
-        newElement.appendChild(nameElement2);
-        
-        const nameElement3 = document.createElement("li");
-        nameElement3.className = "footer__link";
-        nameElement3.textContent = "Andrey Danylyshyn";
-        newElement.appendChild(nameElement3);
-        
-        mainContainer.appendChild(newElement);
-        container.classList.add("clicked");
+    let existingElement = document.querySelector(".new-footer-links");
 
-        
-        container.removeEventListener("click", handleClick);
-    } 
+    if (!existingElement) {
+        let newElement = document.createElement("ul");
+        newElement.className = "footer__links new-footer-links"; 
+
+        const names = ["Oleksandr Nykyforchyn", "Vlad Karanevych", "Andrey Danylyshyn"];
+        names.forEach(name => {
+            const nameElement = document.createElement("li");
+            nameElement.className = "footer__link";
+            nameElement.textContent = name;
+            newElement.appendChild(nameElement);
+        });
+
+        mainContainer.appendChild(newElement);
+    } else {
+        mainContainer.removeChild(existingElement);
+    }
 });
+
+
+
+
+
+
